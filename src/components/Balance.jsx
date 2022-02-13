92be165593a4b15398678f490d33d70890820dfa
Nnamdi <nnamdi.b@gmail.com>
2022-02-13 21:04:47 +0100

    finished balance

--- src/components/Balance.jsx
+++ src/components/Balance.jsx
@@ -1,10 +1,12 @@
-import React, { useState, useEffect, useMemo, useCallback } from 'react';
-import { useReadLocalStorage, useLocalStorage } from 'usehooks-ts';
+import React from 'react';
+import { useReadLocalStorage } from 'usehooks-ts';
 
 function Balance() {
   const history = useReadLocalStorage('history');
 
   const getSumOfIncome = () => {
+    if (!history) return;
+
     const sum = history.reduce(
       (prevValue, currentObject) =>
         currentObject.amount > 0
@@ -17,6 +19,8 @@ function Balance() {
   };
 
   const getSumOfExpenses = () => {
+    if (!history) return;
+
     const sum = history.reduce(
       (prevValue, currentObject) =>
         currentObject.amount < 0
@@ -28,23 +32,39 @@ function Balance() {
     return sum;
   };
 
+  const getTotalBalance = () => {
+    if (!history) return;
+
+    const sum = history.reduce(
+      (prevValue, currentObject) => prevValue + currentObject.amount,
+      0
+    );
+
+    return sum;
+  };
+
   return (
     <div className="balance">
       <h2>Your Balance</h2>
-      <h3>[$260.00]</h3>
+      {history && (
+        <>
+          <h3>{getTotalBalance()}</h3>
 
-      <div>
-        <div>
-          INCOME
-          <br />
-          {getSumOfIncome()}
-        </div>
-        <div>
-          EXPENSE
-          <br />
-          {getSumOfExpenses()}
-        </div>
-      </div>
+          <div>
+            <div>
+              INCOME
+              <br />
+              {getSumOfIncome()}
+            </div>
+            <div>
+              EXPENSE
+              <br />
+              {getSumOfExpenses()}
+            </div>
+          </div>
+        </>
+      )}
+      {!history && <p>Add some transactions to get started</p>}
     </div>
   );
 }
--- src/components/History.jsx
+++ src/components/History.jsx
@@ -7,17 +7,21 @@ function History() {
   //TODO Style the history thats being shown
   return (
     <div>
-      <h2>History</h2>
-      <hr />
-      {history &&
-        history.map((item, i) => (
-          <div className="historyItems" key={i}>
-            <p className="rightBar">
-              {item.text}
-              <span className="historyItemPrice">{item.amount}</span>
-            </p>
-          </div>
-        ))}
+      {history && (
+        <>
+          <h2>History</h2>
+          <hr />
+
+          {history.map((item, i) => (
+            <div className="historyItems" key={i}>
+              <p className="rightBar">
+                {item.text}
+                <span className="historyItemPrice">{item.amount}</span>
+              </p>
+            </div>
+          ))}
+        </>
+      )}
     </div>
   );
 }
